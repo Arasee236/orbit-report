9e13844f5a1f02e84e5fcffd7d35dbb37ed8e3aa
Arasi Arumugam <ezhil.arum@gmail.com>
2021-07-01 23:57:58 -0500

    Searching part completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,11 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList:Satellite[];
+  displayList: Satellite[];
   constructor(){
     this.sourceList = [];
+    this.displayList = [];
+    
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function(response) {
@@ -25,13 +28,31 @@ export class AppComponent {
 
              // TODO: add the new Satellite object to sourceList 
              this.sourceList.push(satellite);
-
+             
           }
+          this.displayList = this.sourceList.slice(0); 
           
-         
+          
  
        }.bind(this));
     }.bind(this));
  
   }
+
+  search(searchTerm: string): void {
+
+    let matchingSatellites:Satellite[] = [];
+    searchTerm=searchTerm.toLowerCase();
+    for(let i=0;i<this.sourceList.length;i++){
+      let name = this.sourceList[i].name.toLowerCase();
+      if(name.indexOf(searchTerm) >= 0){
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+
+    this.displayList = matchingSatellites;
+    
+
+  }
+
 }
